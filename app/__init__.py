2c1bd7a9f52e2b447af1ea8730516cfcbed91489
Terrence Battlehunt <ciscoittech@gmail.com>
2023-09-14 09:10:52 -0400

    inital

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_login import LoginManager
+import mongoengine as me
+from app.models import User
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Connect to the MongoDB database using mongoengine
+me.connect('quicksilver', host='localhost', port=27017)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'  #
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        return User.objects.get(id=user_id)
+    except me.DoesNotExist:
+        return None
+
+
+# Import routes after creating the app instance
+# from app.routes import user_routes, quiz_routes, auth_routes
---
+++ app/models.py
@@ -0,0 +1,17 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import mongoengine as me
+from datetime import datetime
+
+
+class User(UserMixin, me.Document):
+    email = me.StringField(required=True, unique=True)
+    username = me.StringField(required=True, unique=True)
+    password_hash = me.StringField(required=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
